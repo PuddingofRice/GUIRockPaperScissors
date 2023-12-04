c278f64472fd222b8f927b0b86a456a55c771096
Oliver Rice <84247630+PuddingofRice@users.noreply.github.com>
2023-12-04 12:51:42 +0000

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace RockPaperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.picboxUserChoice = new System.Windows.Forms.PictureBox();
+            this.picboxComputerChoice = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxUserChoice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputerChoice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(182, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(773, 86);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rock Paper Scissors Game";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(364, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(428, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Click one of these three buttons:";
+            // 
+            // btnRock
+            // 
+            this.btnRock.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRock.Location = new System.Drawing.Point(213, 191);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(124, 36);
+            this.btnRock.TabIndex = 2;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPaper.Location = new System.Drawing.Point(501, 191);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(131, 36);
+            this.btnPaper.TabIndex = 3;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnScissors.Location = new System.Drawing.Point(798, 191);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(142, 36);
+            this.btnScissors.TabIndex = 4;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // picboxUserChoice
+            // 
+            this.picboxUserChoice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picboxUserChoice.Location = new System.Drawing.Point(63, 287);
+            this.picboxUserChoice.Name = "picboxUserChoice";
+            this.picboxUserChoice.Size = new System.Drawing.Size(463, 325);
+            this.picboxUserChoice.TabIndex = 5;
+            this.picboxUserChoice.TabStop = false;
+            // 
+            // picboxComputerChoice
+            // 
+            this.picboxComputerChoice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picboxComputerChoice.Location = new System.Drawing.Point(615, 287);
+            this.picboxComputerChoice.Name = "picboxComputerChoice";
+            this.picboxComputerChoice.Size = new System.Drawing.Size(447, 325);
+            this.picboxComputerChoice.TabIndex = 6;
+            this.picboxComputerChoice.TabStop = false;
+            this.picboxComputerChoice.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(756, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Computer Choice:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(244, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Your choice:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResult.Location = new System.Drawing.Point(477, 630);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(74, 30);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 683);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.picboxComputerChoice);
+            this.Controls.Add(this.picboxUserChoice);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picboxUserChoice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputerChoice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.PictureBox picboxUserChoice;
+        private System.Windows.Forms.PictureBox picboxComputerChoice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ RockPaperScissors/RockPaperScissors/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            // code below for when player coice is Rock
+            Random rnd = new Random();
+            int CompChoice = rnd.Next(1, 4);
+            lblResult.Text = Convert.ToString(CompChoice);
+
+            picboxUserChoice.BackgroundImage = Properties.Resources.Rock;
+
+            if (CompChoice == 1)
+            {
+                picboxComputerChoice.BackgroundImage = Properties.Resources.Rock;
+                lblResult.Text = Convert.ToString("Result: Draw!");
+            } else if (CompChoice == 2)
+            {
+                picboxComputerChoice.BackgroundImage = Properties.Resources.Paper;
+                lblResult.Text = Convert.ToString("Result: You Loose!");
+            } else if (CompChoice == 3)
+            {
+                picboxComputerChoice.BackgroundImage = Properties.Resources.Scissors;
+                lblResult.Text = Convert.ToString("Result: You Win!");
+            }
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            // code below for when player coice is Paper
+            Random rnd = new Random();
+            int CompChoice = rnd.Next(1, 4);
+            lblResult.Text = Convert.ToString(CompChoice);
+
+            picboxUserChoice.BackgroundImage = Properties.Resources.Paper;
+
+            if (CompChoice == 1)
+            {
+                picboxComputerChoice.BackgroundImage = Properties.Resources.Rock;
+                lblResult.Text = Convert.ToString("Result: You Win!");
+            } else if (CompChoice == 2)
+            {
+                picboxComputerChoice.BackgroundImage = Properties.Resources.Paper;
+                lblResult.Text = Convert.ToString("Result: Draw!");
+            } else if (CompChoice == 3)
+            {
+                picboxComputerChoice.BackgroundImage = Properties.Resources.Scissors;
+                lblResult.Text = Convert.ToString("Result You Loose!");
+            }
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            // code below for when player coice is scissors
+            Random rnd = new Random();
+            int CompChoice = rnd.Next(1, 4);
+            lblResult.Text = Convert.ToString(CompChoice);
+
+            picboxUserChoice.BackgroundImage = Properties.Resources.Scissors;
+
+            if (CompChoice == 1)
+            {
+                picboxComputerChoice.BackgroundImage = Properties.Resources.Rock;
+                lblResult.Text = Convert.ToString("Result: You Loose!");
+            } else if (CompChoice == 2)
+            {
+                picboxComputerChoice.BackgroundImage = Properties.Resources.Paper;
+                lblResult.Text = Convert.ToString("Result: You Win!");
+            } else if (CompChoice == 3)
+            {
+                picboxComputerChoice.BackgroundImage = Properties.Resources.Scissors;
+                lblResult.Text = Convert.ToString("Result: Draw!");
+            }
+        }
+    }
+}
